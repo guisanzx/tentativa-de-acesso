695695d147025c8bcae53a01da4ab2287e030648
guisanzx <guisds1314@gmail.com>
2025-10-04 09:15:30 -0300

    Update Tentativa de Login.py

--- Tentativa de Login.py
+++ Tentativa de Login.py
@@ -1,15 +1,14 @@
 listaLogin = ["joao", "maria", "pedro", "ana", "carlos", "beatriz"]
-listaSenha = ["123", "456", "789", "abc", "def", "ghi"]
+listaSenha = ["123", "456", "789", "r321", "wd.W123", "84WdsW"]
 
 usuarios = {
     "joao": "123",
     "maria": "456",
     "pedro": "789",
-    "ana": "abc",
-    "carlos": "def",
-    "beatriz": "ghi"
+    "ana": "r321",
+    "carlos": "wd.W123",
+    "beatriz": "84WdsW"
 }
-
 
 login = input("Digite seu login: ")
 senha = input("Digite sua senha: ")
